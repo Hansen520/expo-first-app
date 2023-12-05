4a288ed94dc60e544096ba613659fa46e5fb2f81
Hansen1994 <646380243@qq.com>
2023-12-05 23:08:25 +0800

    react

--- src_29_Project/redux/store.ts
+++ src_29_Project/redux/store.ts
@@ -1,6 +1,6 @@
-import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { legacy_createStore as createStore, compose, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import reduxThunk from 'redux-thunk'
 
-const store = createStore(reducers, applyMiddleware(reduxThunk as any))
+const store = createStore(reducers, compose(applyMiddleware(reduxThunk as any) as any))
 export default store
